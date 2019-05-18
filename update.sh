1291051adbf8dcf99506c2a2b9e108d557bc30ca
flymysql <flyphp@outlook.com>
2019-05-18 18:14:27 +0800

    项目更新脚本

---
+++ update.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/flymysql/flybook.git
+cp -rf flybook/app.js ./
+cp -rf flybook/routes ./
+cp -rf flybook/bin ./
+cp -rf flybook/server/mysql/post.js ./server/mysql/post.js
+cp -rf flybook/server/mysql/tags.js ./server/mysql/tags.js
+cp -rf flybook/until ./
+cp -rf flybook/views ./
+cp -rf flybook/config.js ./
+cp -rf flybook/package.json ./
+cp -rf flybook/LICENSE ./
+cp -rf flybook/README.md ./
+cp -rf flybook/update.sh ./
+rm -rf flybook
