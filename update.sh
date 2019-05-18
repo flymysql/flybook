461957363ffbe2d215b356f30e51582fdde3b954
flymysql <flyphp@outlook.com>
2019-05-18 19:12:36 +0800

    update

--- update.sh
+++ update.sh
@@ -5,6 +5,8 @@ cp -rf flybook/bin ./
 cp -rf flybook/server/mysql/post.js ./server/mysql/post.js
 cp -rf flybook/server/mysql/tags.js ./server/mysql/tags.js
 cp -rf flybook/until ./
+cp -rf flybook/public/javascripts ./public/
+cp -rf flybook/public/stylesheets ./public/
 cp -rf flybook/views ./
 cp -rf flybook/config.js ./
 cp -rf flybook/package.json ./
