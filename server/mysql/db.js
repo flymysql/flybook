573f6ee722098be13f3cecce1b82c1856ef651d7
flymysql <flyphp@outlook.com>
2019-05-18 14:34:26 +0800

    db.js

--- server/mysql/db.js
+++ server/mysql/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 
 exports.connection = mysql.createPool({
-    host     : '120.77.183.14',
-    user     : 'www_idealli_com',
-    password : 'www.l975',
-    database : 'www_idealli_com',
+    host     : '',
+    user     : '',
+    password : '',
+    database : '',
     multipleStatements: true
 })
