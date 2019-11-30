645370f9ecbc95f96c3cff6f065980e1dcde56e6
flymysql <flyphp@outlook.com>
2019-11-30 10:35:35 +0800

    更新脚本修改😁

--- update.sh
+++ update.sh
@@ -1,19 +1,4 @@
-git clone https://github.com/flymysql/flybook.git
-cp -rf app.js ../
-cp -rf routes ../
-cp -rf bin ../
-cp -rf controllers ../
-cp -rf server/mysql ../server/
-cp -rf server/install ../server/
-cp -rf server/file ../server/
-cp -rf until ../
-cp -rf public/javascripts ../public/
-cp -rf public/third-part ../public/
-cp -rf public/stylesheets ../public/
-cp -rf public/others ../public/
-cp -rf views ../
-cp -rf package.json ../
-cp -rf LICENSE ../
-cp -rf README.md ../
-rm -rf flybook
-cp -rf update.sh ../
+git clone https://github.com/flymysql/flybook/.git
+sh ./update_list.sh
+cp flybook/update_list.sh ./
+rm flybook
