b47877fd1dad4aea640698e424926f636cfd007e
flymysql <flyphp@outlook.com>
2019-11-30 10:40:11 +0800

    更新脚本修改😁

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ echo "flybook —— 基于node&express的轻量级博客"
 echo "https://github.com/flymysql/flybook/.git"
 echo
 echo "============================================"
-git clone https://github.com/flymysql/flybook/.git
+git clone https://github.com/flymysql/flybook.git
 sh ./update_list.sh
 cp flybook/update_list.sh ./
 rm flybook
