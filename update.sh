9c7364670a133a56af749faeae4ba4e56679e6d8
flymysql <flyphp@outlook.com>
2019-12-01 17:18:08 +0800

    修复文章提交时的div嵌套😁

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+rm -rf flybook
 echo "============================================"
 echo 
 echo "flybook —— 基于node&express的轻量级博客"
@@ -7,4 +8,4 @@ echo "============================================"
 git clone https://github.com/flymysql/flybook.git
 sh ./update_list.sh
 cp flybook/update_list.sh ./
-rm flybook
+rm -rf flybook
