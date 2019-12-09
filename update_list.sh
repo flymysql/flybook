605302ceef1cca710c0f41ecfe291ec6fab88fd8
flymysql <flyphp@outlook.com>
2019-12-09 18:04:12 +0800

    将多个js压缩一起，减少请求次数

--- update_list.sh
+++ update_list.sh
@@ -1,19 +1,20 @@
-cp -rf flybook/app.js ./
-cp -rf flybook/routes ./
-cp -rf flybook/bin ./
-cp -rf flybook/controllers ./
-cp -rf flybook/server/mysql ./server/
-cp -rf flybook/server/config ./server/
-cp -rf flybook/server/file ./server/
-cp -rf flybook/until ./
+# cp -rf flybook/app.js ./
+# cp -rf flybook/routes ./
+# cp -rf flybook/bin ./
+# cp -rf flybook/controllers ./
+# cp -rf flybook/server/mysql ./server/
+# cp -rf flybook/server/config ./server/
+# cp -rf flybook/server/file ./server/
+# cp -rf flybook/until ./
 cp -rf flybook/public/javascripts ./public/
-cp -rf flybook/public/third-part ./public/
+# cp -rf flybook/public/third-part ./public/
 cp -rf flybook/public/stylesheets ./public/
-cp -rf flybook/public/others ./public/
-cp -rf flybook/public/robots.txt ./public/
+# cp -rf flybook/public/others ./public/
+# cp -rf flybook/public/robots.txt ./public/
 cp -rf flybook/views ./
-cp -rf flybook/package.json ./
-cp -rf flybook/LICENSE ./
-cp -rf flybook/README.md ./
+# cp -rf flybook/package.json ./
+# cp -rf flybook/LICENSE ./
+# cp -rf flybook/README.md ./
 cp -rf flybook/update.sh ./
 cp -rf flybook/update_list.sh ./
+npm run pmrestart
