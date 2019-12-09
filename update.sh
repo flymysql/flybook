cc65dd464269942c66b9a29a11858dd53304ea50
flymysql <flyphp@outlook.com>
2019-12-09 18:15:22 +0800

    升级模式修改👌

--- update.sh
+++ update.sh
@@ -1,10 +1,15 @@
-rm -rf flybook
 echo "============================================"
 echo 
 echo "flybook —— 基于node&express的轻量级博客"
 echo "https://github.com/flymysql/flybook/.git"
 echo
 echo "============================================"
+
+if [ ! -d "flybook" ]; then
 git clone https://github.com/flymysql/flybook.git
+else
+cd flybook
+git pull
+cd ..
+fi
 sh flybook/update_list.sh
-rm -rf flybook
